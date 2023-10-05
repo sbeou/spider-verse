727c0feaff3a0312e705edcbfa91fdb34b3c67eb
sbeou13 <design@walf.com.br>
2023-10-05 08:41:48 -0300

    Fix erro domain origin

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   env: {
     API_URL: "https://64a54c6300c3559aa9bf7245.mockapi.io",
-    DOMAIN_ORIGIN: "http://localhost:3000",
+    //DOMAIN_ORIGIN: "http://localhost:3000",
+    DOMAIN_ORIGIN: "spiderverse-hhzaw3059-sebastien-guilets-projects.vercel.app",
   },
 };
 
